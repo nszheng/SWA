c3280834d25550a43c20aa2cab15b2497fb9385c
nszheng <47682281+nszheng@users.noreply.github.com>
2019-02-16 00:17:39 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="Smith-Waterman Algorithm",
+    version="1.0",
+    author_email="neil.zheng@yale.edu",
+    description="Basic implementation of Smith-Waterman Algorithm",
+    py_modules = ['sw']
+)
